f6391a3c5ee049c9784a3b174c26d3abc6d6bf0d
ProgrammerJOHA <akhadovjakhongir65@gmail.com>
2023-10-21 10:13:08 +0500

    Doston bot

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { Pages } from './Pages';
 function App() {
   return (
     <div className="App">
-        {/* Hello Diyor doston  */}
-        {/* Helo joha bot */}
+        {/* San dostonchik chik chik botsan ha ha ha codlarni figmadan olganingni domlga aytib beraymi bot */}
       <div style={{width: '100%', height: '100%', position: 'relative', background: 'white'}}>
     <div style={{width: 1920, height: 50, left: 0, top: 0, position: 'absolute'}}>
         <div style={{width: 1920, height: 50, left: 0, top: 0, position: 'absolute', background: '#EFEFEF'}} />
